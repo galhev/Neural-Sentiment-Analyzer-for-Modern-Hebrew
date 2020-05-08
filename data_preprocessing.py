403b0342efc396a8a515b8a1f4a43191501585e4
galhev <galhever@gmail.com>
2020-05-08 16:46:47 +0300

    Add files via upload

---
+++ data_preprocessing.py
@@ -0,0 +1,54 @@
+from torchtext import data
+import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+
+
+def labels_distribution(y):
+    fig = plt.figure(figsize=(8,5))
+    ax = sns.barplot(x=y.unique(),y=y.value_counts());
+    ax.set(xlabel='Labels')
+    print(y.value_counts())
+
+
+def cleanup_text(texts):
+    cleaned_text = []
+    for text in texts:
+        # remove punctuation
+        text = re.sub('[!#?,.:";]', ' ', text)
+        # remove multiple spaces
+        text = re.sub(r' +', ' ', text)
+        # remove newline
+        text = re.sub(r'\n', ' ', text)
+        cleaned_text.append(text)
+    return cleaned_text
+
+
+def get_files(path, dev_size, max_document_length, seed, data_type, tokenizer):
+    # include_lengths = True - This will cause batch.text to now be a tuple with the first element being our sentence (a numericalized tensor that has been padded) and the second element being the actual lengths of our sentences.
+    Text = data.Field(preprocessing=cleanup_text, tokenize=tokenizer, batch_first=True, include_lengths=True, fix_length=max_document_length) # fix_length - make the sentences padded in the same lengths for all the batches
+    Label = data.Field(sequential=False, use_vocab=False, pad_token=None, unk_token=None)
+
+    # All files:
+    fields = [('text', Text), ('labels', Label)]
+
+    train_data, test_data = data.TabularDataset.splits(
+        path=path,
+        train= data_type + '_train.tsv',
+        test= data_type + '_test.tsv',
+        format='tsv',
+        fields=fields,
+        skip_header=False
+    )
+
+    train_data, valid_data = train_data.split(split_ratio=dev_size, random_state=random.seed(seed))
+    print(f'Number of training examples: {len(train_data)}')
+    print(f'Number of validation examples: {len(valid_data)}')
+    print(f'Number of testing examples: {len(test_data)}')
+    return train_data, valid_data, test_data, Text, Label
+
+
+
+
+
